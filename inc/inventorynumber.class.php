baae410aff454163c945a849a7b25fef5a44461d
Pedro Rocha <jpnr79@gmail.com>
2025-12-14 18:15:38 +0000

    Implement install method for inventory number class with migration placeholder

--- inc/inventorynumber.class.php
+++ inc/inventorynumber.class.php
@@ -1,3 +1,10 @@
+  /**
+   * Install or migrate plugin database schema for this class
+   * @param Migration $migration
+   */
+  public static function install($migration) {
+    // No migration logic needed for inventory numbers yet
+  }
 <?php
 /*
  -------------------------------------------------------------------------
